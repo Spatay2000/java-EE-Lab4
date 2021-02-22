cfea38b0d609c771485a77bf6692ccc4495eceb8
Spatay2000 <Spatay.148@gmail.com>
2021-02-22 15:33:20 +0600

    URL

---
+++ src/main/java/URLFirstServlet.java
@@ -0,0 +1,27 @@
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+public class URLFirstServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response){
+        try{
+
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+
+            String n=request.getParameter("userName");
+            out.print("Welcome "+n);
+
+            //appending the username in the query string
+            out.print("<a href='urlservlet2?usname="+n+"'>visit</a>");
+
+            out.close();
+
+        }catch(Exception e){System.out.println(e);}
+    }
+
+}
+
